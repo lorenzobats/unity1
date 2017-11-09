0017b5101af17433b00bed7178400a26a4f1d447
Lorenzo Battiston <lorenzo.battiston@r132-04-w10.SCHULE-ISERV>
2017-11-09 14:40:39 +0100

    second com

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -31,6 +31,8 @@ public class DestroyByContact : MonoBehaviour
 		if (other.tag == "Player")
 		{
 			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+			gameController.GameOver ();
+
 		}
 		gameController.AddScore (scoreValue);
 		Destroy(other.gameObject);
